c503217cb9cd1413160631775d87884f270555e7
starkenterprise <rowan.stark@hotmail.com>
2016-10-10 13:22:48 +0800

    Update main.c

--- 06-lift-model/Source/main.c
+++ 06-lift-model/Source/main.c
@@ -1,9 +1,14 @@
 //#include <msp430fr5739.h>
+//EDIT: Have made enum bool for button state and added clarity for if loops. Should work on a larger range of boards now.
 #include "lift1lib.h"
+
+#define LEDon 0x20
+#define LEDoff 0x00
 
 int main()
 {
-    uc_8 buttonstat, old_lvl, current_lvl;
+    uc_8 buttonstat, old_lvlstat, current_lvlstat;
+    enum bool flag;
 	//1)Initialising the lift system
     Initialise();
 	liftInit();
@@ -16,43 +21,37 @@ int main()
 	//5)Switching on or off a specified LED associated with buttons etc
 	//6)Delaying an approximate time period
 	//7)Opening and closing the lift doors (simulated by time delay operations)
-   
-    turnLEDsON();
-    Delay1(2500);
-    turnLEDsOFF();
-    moveInitiPos();	//move lift cage to initial pos
-    
     while (1)
     {
 	/*
-	if(floorGet(&current_lvl))
+	if(floorGet(&current_lvlstat))
 	{
 		
 	}
 	*/
-		if(buttonGet(&buttonstat))
+      flag=buttonGet(&buttonstat);
+		if(flag==true)
 		{
-			WriteLed1(buttonstat, LEDon);
-			if (buttonstat%2==0)
+			//WriteLed1(6, LEDon);
+			if (buttonstat%2==1)
 			{
 				lift1Up();
 				//Delay1(25000000);
 				//lift1Stop();
-
-				while(!floorGet(&current_lvl)){}
+				while(floorGet(&current_lvlstat)==false){}
 				lift1Stop();
 			}
-			else
+			else if  (buttonstat%2==0)
 			{
 				lift1Down();
 				//Delay1(25000000);
 				//lift1Stop();
-				while(!floorGet(&current_lvl)){}
+				while(floorGet(&current_lvlstat)==false){}
 				lift1Stop();
 			}
 		}
 	//Delay1(25000000);
-	WriteLed1(buttonstat, LEDoff);
+	//WriteLed1(buttonstat, LEDoff);
     }
 	return 0;
 }
